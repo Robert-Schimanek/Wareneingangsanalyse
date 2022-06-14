b59d0589060addfe77d11606d14553688ddfaeb4
Tenny131 <ibossgamerhd@gmail.com>
2022-06-14 19:41:54 +0200

    keine neuen Spalten hinzufuegen
    und noch etwas kommentiert

--- prepare Dataset.py
+++ prepare Dataset.py
@@ -3,46 +3,34 @@ from joblib import load
 import numpy as np
 import pandas as pd
 
+#TODO: Datensatz muss noch nach Datum sortiert werden
+#TODO: Nur Spalten loeschen die bei RealPartNo leer sind
 
 #CSV Dateien einlesen
 dataset = load('C:/Users/gezer/Desktop/Wareneingangsanalyse/tidydataframe.sav')
-df = pd.DataFrame(dataset)
-newcolumn = pd.DataFrame(dataset)
 export = pd.read_csv('https://raw.githubusercontent.com/Tenny131/Wareneingangsanalyse/main/export.csv')
 filter = pd.read_csv('https://raw.githubusercontent.com/Tenny131/Wareneingangsanalyse/main/filter%20list.csv')
-print(df); print(newcolumn)
+df = pd.DataFrame(dataset)
 
 # Leerzeichen von Real PartNo entfernen
 df['Real PartNo'] = df['Real PartNo'].str.replace(" ","")
-newcolumn['Real PartNo'] = newcolumn['Real PartNo'].str.replace(" ","")
 
 # Unwichtige Spalten loeschen und neue spalte importieren
 df.drop(df.columns.difference(['Customer Number', 'Real PartNo', 'Delivery ID', 'Customer Delv No.', 'Product Group', 'Bar Code', 'Box exists', 'Box code scanable']), axis=1, inplace=True)
-newcolumn.drop(newcolumn.columns.difference(['Box code', 'Box number selected', 'CBN', 'Effective Reman']), axis=1, inplace=True)
-print(df);  print(newcolumn)
-
-# Datensatz mit neue Spalte zusammenfuegen
-dfnew = pd.concat([df,newcolumn], axis=1)
+print(df)
 
 # Spalten mit leeren Inhalten loeschen
-dfnew.replace('', np.nan, inplace=True)
-dfnew.dropna(inplace=True)
-print(dfnew)
+df.replace('', np.nan, inplace=True)
+df.dropna(inplace=True)
+print(df)
 
-
-# Datein nach Real PartNo filtern
+# Datein nach Real PartNo filtern <100 werden geloscht
 list = filter['Real PartNo'].to_list();   #print(list)
 for i in list:
-    filtered = dfnew.loc[(dfnew['Real PartNo'] == i)]; #print(k)
+    filtered = df.loc[(df['Real PartNo'] == i)]; #print(k)
     export = pd.concat([export,filtered])
 export.drop(export.index[[0]], inplace=True)
 print(export)
-
-# Duplikate entfernen
-#export = export.drop_duplicates(keep=False)
-
-# Datensatz durchmischen
-#export = export.sample(frac=1)
 
 # Datensatz in X und Y Anteile aufteilen
 exportX = export.drop('Real PartNo', axis=1)
@@ -52,8 +40,6 @@ exportY = export.drop(export.columns.difference(['Real PartNo']), axis=1)
 exportX.to_csv('C:/Users/gezer/Desktop/Wareneingangsanalyse/DatasetX.csv', index=False)
 exportY.to_csv('C:/Users/gezer/Desktop/Wareneingangsanalyse/DatasetY.csv', index=False)
 
-'''
 # Real PartNo zaehlen
-dups = filtered.pivot_table(columns=['Real PartNo'], aggfunc='size')
-dups.to_csv('C:/Users/gezer/Desktop/duplicates.csv', index=True)
-'''
+#dups = filtered.pivot_table(columns=['Real PartNo'], aggfunc='size')
+#dups.to_csv('C:/Users/gezer/Desktop/duplicates.csv', index=True)
